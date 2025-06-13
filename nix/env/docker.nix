1a3c38029a3e1c92acdaf4c78ca26f6e8746d75e
Qiming Chu <cchuqiming@gmail.com>
2025-06-13 22:53:14 +0800

    nix(docker): refactor docker image configuration
    Signed-off-by: Qiming Chu <cchuqiming@gmail.com>

--- nix/env/docker.nix
+++ nix/env/docker.nix
@@ -13,39 +13,35 @@
   ieda,
 }:
 let
-  self = pkgs.dockerTools.buildLayeredImage {
-    name = "ieda-perf-docker";
-    tag = "latest";
-    contents = [
-      git
-      file
-      which
-      coreutils
-      gnused
-      nix
-      wget
-      curl
+  self = pkgs.dockerTools.buildImage {
+    name = "ieda-docker";
+    tag = "25-05-13";
+    copyToRoot = pkgs.buildEnv {
+      name = "image-root";
+      paths = with pkgs; [
+        git
+        file
+        which
+        neovim
+        coreutils
+        gnused
+        wget
+        curl
+        nix
+        ieda
 
-      bashInteractive
-      dockerTools.binSh
-      dockerTools.usrBinEnv
-
-      ieda
-    ];
-
-    enableFakechroot = true;
-    fakeRootCommands = ''
-      mkdir -p /perfspace
-    '';
+        bashInteractive
+        dockerTools.binSh
+        dockerTools.usrBinEnv
+      ];
+      pathsToLink = [ "/bin" ];
+    };
     config = {
       Env = [
         "NIX_PAGER=cat"
-        # A user is required by nix
-        # https://github.com/NixOS/nix/blob/9348f9291e5d9e4ba3c4347ea1b235640f54fd79/src/libutil/util.cc#L478
         "USER=nobody"
         "EDITOR=nvim"
       ];
-      WorkingDir = "/perfspace";
     };
   };
 in
